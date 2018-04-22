abd55fde072080337ca1b0db13612637697f6355
maxime blanc <maximeblanc.dev@gmail.com>
2018-04-22 18:44:52 +0200

    wip

--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Provider } from 'mobx-react'
-
-import 'normalize.css'
-import './index.css'
 
 import App from './App'
-import stores from './stores/RootStore'
 
-ReactDOM.render(
-  <Provider {...stores}>
-    <App />
-  </Provider>,
-  document.getElementById('app')
-)
+ReactDOM.render(<App />, document.getElementById('app'))
